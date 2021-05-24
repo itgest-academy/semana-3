6dd48e8f82220647b322c49489a62cf393c08524
nelsonmatositgest <nelson.matos@ideiasdinamicas.com>
2021-05-24 16:37:54 +0100

    exercicios semana 3

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <Product
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+          :index="index" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Product from '@/components/Product.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Product
+  },
+
+  data () {
+    return {
+      products: [
+        {
+          id: 1,
+          photo: '',
+          name: 'Rato',
+          quantity: 2,
+          price: 10
+        },
+        {
+          id: 2,
+          photo: '',
+          name: 'Teclado 2',
+          quantity: 10,
+          price: 15
+        },
+        {
+          id: 3,
+          photo: '',
+          name: 'Monitor 2',
+          quantity: 2,
+          price: 200
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="col">
+    <div class="card shadow-sm">
+      <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      <div class="card-body">
+        <h2>{{ index + 1 }} - {{ product.name }}</h2>
+        
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <span>{{ product.quantity }}</span>
+          </div>
+          <small class="text-muted">{{ product.price }} €</small>
+        </div>
+
+        <strong>{{ totalPrice }}</strong>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  props: {
+    product: {
+      type: Object,
+      required: true
+    },
+
+    index: {
+      type: Number
+    }
+  },
+
+  computed: {
+    totalPrice () {
+      return this.product.quantity * this.product.price
+    }
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  watch: {
+  },
+
+  created () {
+  },
+
+  mounted () {
+  },
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
